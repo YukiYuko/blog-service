c9407dbd9e7a615df7b19d3ab07b9402c5b79e28
project-admin <790684386@qq.com>
2019-01-03 19:24:14 +0800

    update

--- controller/System.js
+++ controller/System.js
@@ -5,65 +5,57 @@ const TagsSchema = new Schema({
   newsId: {type: Schema.Types.ObjectId, ref: 'News'},
   name:{type:String},
 }, { timestamps: true });
+const TypesSchema = new Schema({
+  newsId: {type: Schema.Types.ObjectId, ref: 'News'},
+  name:{type:String},
+  key:{type:String},
+}, { timestamps: true });
 const Tags = mongoose.model('Tags', TagsSchema);
+const Types = mongoose.model('Types', TypesSchema);
 
-// 查询标签
-const findTag = (name) => {
-  return new Promise((resolve, reject) => {
-    Tags.findOne({name}, (err, doc) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(doc);
-    });
-  });
-};
-// 查询所有标签
-const findALLTag = (name) => {
-  return new Promise((resolve, reject) => {
-    Tags.find({}, (err, doc) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(doc);
-    });
-  });
-};
-// 创建标签
+// 创建标签或类型
 const createTag = async (ctx) => {
-  let tag = new Tags({
-    name: ctx.request.body.name
-  });
-  let doc = await findTag(tag.name);
-  console.log("doc", doc)
+  let model = "";
+  let type = ctx.request.body.type * 1 || 1;
+  let doc = "";
+  if (type === 1) {
+    model = new Tags({
+      name: ctx.request.body.name
+    });
+    doc = await Tags.findOne({name: model.name});
+  } else if (type === 2) {
+    console.log("ctx.request.body.key", ctx.request.body.key)
+    model = new Types({
+      name: ctx.request.body.name,
+      key: ctx.request.body.key
+    });
+    doc = await Types.findOne({name: model.name});
+  }
   if (doc) {
-    console.log("该标签已存在");
     ctx.status = 200;
     ctx.body = {
       code: 403000,
       info: status[403000]
     };
   } else {
-    await new Promise((resolve, reject) => {
-      tag.save((err) => {
-        if (err) {
-          reject(err);
-        }
-        resolve();
-      });
-    });
-    console.log('添加成功');
+    await model.save();
     ctx.status = 200;
     ctx.body = {
       code: 200,
       info: status[200],
-      data: tag
+      data: model
     }
   }
 };
-// 查询所有标签
+// 查询所有标签或类型
 const listTag = async (ctx) => {
-  let doc = await findALLTag();
+  let type = ctx.request.query.type * 1 || 1;
+  let doc = "";
+  if (type === 1) {
+    doc = await Tags.find({});
+  } else if (type === 2) {
+    doc = await Types.find({});
+  }
   if (doc) {
     ctx.status = 200;
     ctx.body = {
